aa37ae12bcff5fa6621ecc422d55584b711a6003
locatti22 <locattifelipe@gmail.com>
2025-12-06 23:39:41 -0300

    Alteração na regra dos números

--- scripts.js
+++ scripts.js
@@ -4,9 +4,14 @@ function generateNumber() {
     const minValue = Math.ceil(document.querySelector('.input-min').value)
     const maxValue = Math.floor(document.querySelector('.input-max').value)
 
-    const result = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+    if( minValue >= maxValue) {
+        alert('O valor mínimo tem que ser MENOR do que o valor máximo')
+    } else {
+        const result = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
 
     alert(result)
+    }
+
 }
 
 activateButton.addEventListener('click', generateNumber)
